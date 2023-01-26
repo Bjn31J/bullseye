89e6a069dfb690bad251bc115e916277ebb2012a
Benjamin Jaramillo <200308335@itcelaya.edu.mx>
2023-01-25 21:09:57 -0600

    Este es mi mensaje del primer commit.

--- bullseye/ViewController.swift
+++ bullseye/ViewController.swift
@@ -13,7 +13,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showAlert(){
+        let alert =  UIAlertController(
+            title: "! Puro XCode alv !",
+            message: " Mi primera aplicación amonosssss",
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: " Salu2 ",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+    }
 
 }
 
