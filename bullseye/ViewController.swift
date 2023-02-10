5db55033529e3971790c55666ed7f102c2384932
Benjamin Jaramillo <200308335@itcelaya.edu.mx>
2023-02-09 20:10:28 -0600

    Tercer progrma

--- bullseye/ViewController.swift
+++ bullseye/ViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 50
-
+    var targetValue = 0
+    var currentValue: Int = 25
+    @IBOutlet  var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
         // Do any additional setup after loading the view.
     }
     @IBAction func showAlert(){
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
         
         let alert =  UIAlertController(
             title: "! Puro XCode alv !",
@@ -30,11 +35,23 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert,animated: true,completion: nil)
+        startNewRound()
     }
     @IBAction func sliderMoved(_ slider: UISlider){
         currentValue = lroundf(slider.value)
         
     }
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+    }
+    
+
 
 }
 
